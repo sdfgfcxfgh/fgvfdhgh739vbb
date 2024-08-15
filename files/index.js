ac95e926abb040f9f8a82c8d69ea352db9e264a5
sdfgfcxfgh <yhnhjhgghhhjj@proton.me>
2024-08-15 09:22:51 +0000

    Update index.js

--- files/index.js
+++ files/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const { exec, execSync } = require('child_process');
 const port = process.env.SERVER_PORT || process.env.PORT || 5000;        
-const UUID = process.env.UUID || 'c7ce02ab-7bb9-4153-8303-9a2ca4357ead'; //若需要改UUID，需要在config.json里改为一致
+const UUID = process.env.UUID || '7a03517b-edc8-4e6e-8045-ab2e3310017d'; //若需要改UUID，需要在config.json里改为一致
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '5ddVS93Eq0Uc9he880';
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'fcuist.jhjgtrmvvdbgd.cf';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiNTIwZmQxYWNjNjhjNDU3MzI0MTE1ZjAzYWY3YzA5YTIiLCJ0IjoiOTM1ZmJjMjUtZDU2Ni00YmM1LTg0NzctMTEyNWNmYTNhZDU4IiwicyI6IlptUTFNMkUzTlRFdFpEazBaUzAwTXpGbUxUZ3hPR010TWpneE5XTTFNalEwT0dRMSJ9';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'gfdfghy.uugg.biz.id';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiYjBkMjQ0MTE1NTNkZGZkMzRmNjMxYThkOTNhOTc4OTEiLCJ0IjoiMzc4YzI0NjEtMjAyNy00YzE2LTg2MDctMzllYWI2NGE0NmM4IiwicyI6Ik9XVXlOelZqTm1FdE1XVXpOaTAwTWpBd0xUaGxaV1V0T1dNNU4ySmxZMlptT1dJNSJ9';
 const CFIP = process.env.CFIP || 'na.ma';
 const NAME = process.env.NAME || 'Choreo';
 
